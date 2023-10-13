e492f078ef17eb508576ab94a9e0524e02a562e0
Orsius <orsius@users.noreply.github.com>
2023-10-13 14:21:29 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.4"
 
   backend "s3" {
-    key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "tf-state-storage-a1e1e6a446badf3f0570560f1e81be127a6a7d75"  # the bucket
-    region = "us-east-2"                                                  # the region
+    key    = "github-actions-cicd/terraform.tfstate"           # the directory/file.tfstate
+    bucket = "tf-state-storage-eu-central-1-github-project99"  # the bucket
+    region = "eu-central-1"                                    # the region
   }
 }
